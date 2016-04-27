3d56ea8046bd553353d7a8c03e796fefcec48a9b
eloylp <eloy@sandboxwebs.com>
2016-04-28 00:23:58 +0200

    testing batches

--- controllers/batches.js
+++ controllers/batches.js
@@ -2,37 +2,13 @@ var models = require('../models/models');
 var uuid = require('node-uuid');
 
 
-exports.load = function (req, res, next, batchId) {
-
-    var node_id_header = req.app.get('config')['dds_node_id_header'];
-    var resultCallback = function (err, results) {
-
-        if (results) {
-            req.batch = results;
-            next();
-        } else if (err) {
-            next(err);
-        } else {
-            err = new Error("resource not found");
-            err.status = 404;
-            next(err);
-        }
-    };
-
-    models.message.find({
-            batch_id: batchId,
-            from_node_id: req.header(node_id_header)
-        }).sort({create_time: -1})
-        .exec(resultCallback);
-
-};
-
-exports.create = function(req, res, next){
+/// TODO test it
+exports.create = function (req, res, next) {
 
     var node_id_header = req.app.get('config')['dds_node_id_header'];
     var batch_id = uuid.v4();
     var date = new Date();
-    req.body.forEach(function(item, index, array){
+    req.body.forEach(function (item, index, array) {
 
         array[index].from_node_id = node_id_header;
         array[index].batch_id = batch_id;
@@ -40,21 +16,22 @@ exports.create = function(req, res, next){
         array[index].create_time = date;
     });
 
-    models.message.insertMany(req.body, function(err, docs){
+    models.message.insertMany(req.body, function (err, docs) {
 
-        if(err){
+        if (err) {
             next(err);
-        }else{
+        } else {
             res.json({batch_id: batch_id});
         }
     });
 };
 
+/// TODO test it
 exports.delete = function (req, res, next) {
 
     var node_id_header = req.app.get('config')['dds_node_id_header'];
     models.message.remove({
-            batch_id: req.batch_id,
+            batch_id: req.params.batch_id,
             from_node_id: req.header(node_id_header)
         },
         function (err, results) {
@@ -66,10 +43,26 @@ exports.delete = function (req, res, next) {
         });
 };
 
+// TOdo tested
 exports.show = function (req, res) {
-    res.json(req.batch);
+
+    var max_config_limit = req.app.get('config')['max_pull_messages_allowed'];
+    var node_id_header = req.app.get('config')['dds_node_id_header'];
+
+    models.message
+        .find({to_node_id: req.header(node_id_header), batch_id: req.params.batch_id})
+        .sort({create_time: -1})
+        .limit(max_config_limit)
+        .exec(function (err, results) {
+            if (err) {
+                next(err);
+            } else {
+                res.json(results);
+            }
+        });
 };
 
+// Todo tested
 exports.obtain = function (req, res, next) {
 
     // Todo implement limit, change it to in process and serve.
@@ -78,7 +71,6 @@ exports.obtain = function (req, res, next) {
     var limit = (req.query.limit <= max_config_limit ? req.query.limit : false) || max_config_limit;
     models.message
         .find({to_node_id: req.header(node_id_header), status: "pending"})
-        //.select("_id data")
         .sort({create_time: -1})
         .limit(limit)
         .exec(function (err, results) {
@@ -112,7 +104,7 @@ exports.obtain = function (req, res, next) {
 exports.ack = function (req, res, next) {
 
     var node_id_header = req.app.get('config')['dds_node_id_header'];
-    models.message.update({batch_id: req.body.batch_id, to_node_id: req.header(node_id_header)},
+    models.message.update({batch_id: req.params.batch_id, to_node_id: req.header(node_id_header), status: "processing"},
         {$set: {status: "processed", proccesed_time: new Date()}}, {multi: true})
         .exec(function (err, result) {
 
--- routes/batches.js
+++ routes/batches.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 var batchController = require('../controllers/batches');
 
-router.param('batchId', batchController.load);
 router.post('/', batchController.create);
 router.get('/', batchController.obtain);
-router.get('/:batchId([0-9a-f-]+)', batchController.show);
-router.patch('/ack/:batchId([0-9a-f-]+)', batchController.ack);
+router.get('/:batch_id([0-9a-f-]+)', batchController.show);
+router.patch('/ack/:batch_id([0-9a-f-]+)', batchController.ack);
 
 module.exports = router;
