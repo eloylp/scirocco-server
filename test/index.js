565f879e36678304e385dad1d632bb6479449205
eloylp <eloy@sandboxwebs.com>
2016-05-19 23:40:43 +0200

    added authenthicated test

--- test/index.js
+++ test/index.js
@@ -7,7 +7,6 @@ var server = supertest.agent('http://localhost:3000');
 
 describe('Testing rest api.', function(){
 
-
     it("Should return 403 because there a are not token set.", function(done){
 
         server.get('/')
@@ -19,5 +18,18 @@ describe('Testing rest api.', function(){
                 }
                 done();
             });
+    });
+
+    it("Should return 200 because is authenticated.", function(done){
+      server.get('/')
+          .set('Authorization', 'DEFAULT_TOKEN')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res){
+              if(err){
+                  throw err;
+              }
+              done();
+          });
     });
 });
