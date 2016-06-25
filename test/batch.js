989feb72334c86a132314917332b6531c1bbe0da
eloylp <eloy@sandboxwebs.com>
2016-06-25 20:29:17 +0200

    batch incompletye transfer s commit

--- test/batch.js
+++ test/batch.js
@@ -9,7 +9,7 @@ var model = require('../models/models');
 
 var request = supertest.agent('http://localhost:' + process.env.APP_PORT);
 var server;
-var path = '/queue';
+var path = '/batch';
 var config = require('../test/config');
 
 
@@ -27,7 +27,47 @@ describe('Testing batch resource.', function () {
         });
     });
 
+    it("Should create a new batch for messages posted. It must return a batch object info.", function (done) {
 
-    
+        var batch1 = [
+            {
+                to_node_id: "",
+                data: {test: "testing"}
+            },
+            {
+                to_node_id: "",
+                data: {test: "testing"}
+            },
+            {
+                to_node_id: "",
+                data: {test: "testing"}
+            }
+        ];
 
+        request.post(path)
+            .set('Authorization', config.token)
+            .set(config.fromHeader, config.fromHeaderValue)
+            .set('Content-Type', 'application/json')
+            .send(batch1)
+            .expect(201)
+            .expect('Location',/\/batch\/[0-9a-f]/)
+            .end(function(err, res){
+
+                (res.body).should.be.instanceOf(Object);
+                (res.body.batch_id).should.be.equal(res.header.location.split("/")[2]);
+                (res.body.message_num).should.be.exactly(3)
+                (res.body.messages).should.be.instanceOf(Array);
+                (res.body.n).sould.be.equal(3);
+                done()
+            });
+    });
+
+    it("Should return all batches pendient for actual node", function(done){
+
+        
+
+
+
+
+    });
 });
