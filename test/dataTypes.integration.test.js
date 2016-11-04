e786af8e3240a74a5dee993ccbb597b925c2be66
eloylp <eloy@sandboxwebs.com>
2016-11-04 18:39:51 +0100

    Adding tests for sata type , unstable commit.

--- test/dataTypes.integration.test.js
+++ test/dataTypes.integration.test.js
@@ -7,6 +7,8 @@ var supertest = require('supertest');
 var should = require('should');
 var fs = require('fs');
 var path = require('path');
+var model = require('../models/models');
+
 
 var request = supertest.agent('http://localhost:' + process.env.SCIROCCO_PORT);
 var server;
@@ -16,13 +18,17 @@ var config = require('../config');
 describe('Testing api data types support.', function () {
 
 
-    beforeEach(function () {
+    beforeEach(function (done) {
         delete require.cache[require.resolve('../bin/www')];
         server = require('../bin/www');
+        model.message.remove({}, done);
+
     });
 
     afterEach(function (done) {
-        server.close(done);
+        server.close(function () {
+            model.message.remove({}, done);
+        });
     });
 
     it("Should send a pdf file and get it without errors.", function (done) {
@@ -33,13 +39,14 @@ describe('Testing api data types support.', function () {
             .set('Content-Type', 'application/octet-stream')
             .set(config.headers.from, 'af123')
             .set(config.headers.to, "af123")
-            .send(fs.createReadStream(path.join(__dirname, '/fixtures/tux.pdf')))
+            .send(fs.readFileSync(path.join(__dirname, '/fixtures/tux.pdf')))
             .expect(201)
             .end(function (err, res) {
                 if (err) {
                     throw err;
                 }
-                console.log(res);
+
+                /// TODO MAKE  request to get the message and --> fs.writeFile(path.join(__dirname, '/fixtures/tux2.pdf'), res.body);
                 done();
             });
 
